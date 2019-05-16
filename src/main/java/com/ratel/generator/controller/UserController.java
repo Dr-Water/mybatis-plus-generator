de4983c7d6da2ba7e0ffacde1577b7f11b10b4e3
fuyongnan <ratelfu@163.com>
2019-05-16 10:08:20 +0800

    增加@PathVarible和@RequestParam和swagger集成的区别注解

--- src/main/java/com/ratel/generator/controller/UserController.java
+++ src/main/java/com/ratel/generator/controller/UserController.java
@@ -4,6 +4,7 @@ package com.ratel.generator.controller;
 import com.ratel.generator.entity.User;
 import com.ratel.generator.service.IUserService;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -46,7 +47,7 @@ public class UserController {
 
     @ApiOperation(value="根据用户名获取用户", notes="根据用户的姓名获取用户")
     @GetMapping("/selectByName/{name}")
-    public User selectByName(@PathVariable(required = false) String name) {
+    public User selectByName(@PathVariable String name) {
         System.out.println(name);
         User user = iUserService.selectByName(name);
         return user;
@@ -57,6 +58,42 @@ public class UserController {
     @ApiOperation("根据用户名获取用户")
     @GetMapping("/selectByName2")
     public User selectByName2(@RequestParam(required = false) String name) {
+        User user = iUserService.selectByName(name);
+        return user;
+    }
+
+
+    @ApiOperation(value="根据用户名获取用户", notes="根据用户的姓名获取用户")
+    @ApiImplicitParam(name = "name", value = "用户名",required = false, dataType = "String" ,paramType="query" )
+    @GetMapping("/selectByName3")
+    public User selectByName3(@RequestParam String name) {
+        User user = iUserService.selectByName(name);
+        return user;
+    }
+
+
+
+
+    //使用@PathVariable注解进行传递参数，可方法的参数列表中以不用加required = false,  但是 @ApiImplicitParam中paramtype必须是path，否则无法传参
+    @ApiOperation(value="根据用户名获取用户", notes="根据用户的姓名获取用户")
+    @ApiImplicitParam(name = "name", value = "用户名",required = false, dataType = "String" ,paramType="path" )
+    @GetMapping("/selectByName4/{name}")
+    public User selectByName4(@PathVariable String name) {
+        User user = iUserService.selectByName(name);
+        return user;
+    }
+
+
+    //@ApiImplicitParam中的required=true控制的是Swagger页面的参数不能为空，这时点击try it  out是不能发送请求的，使用这个注解必须将paramType这个参数加上去，否则无法拼接参数
+    //@RequestParam(required = false)控制的是url里的参数可以为空，如果传参会动态拼接在url的后面
+    @ApiOperation(value="根据用户名获取用户", notes="根据用户的姓名获取用户")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "用户名",required = false, dataType = "String" ,paramType="query" ),
+            @ApiImplicitParam( name = "id", value = "用户id", required = true, dataType = "String",paramType="query" )
+    })
+    @GetMapping("/selectByName5")
+    public User selectByName5(@RequestParam(required = false) String name, @RequestParam(required = false) String id) {
+        System.out.println(id);
         User user = iUserService.selectByName(name);
         return user;
     }
